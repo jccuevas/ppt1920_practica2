e7a4c3f195d89b547c39d9269149e7dbfcfd5706
Juan Carlos Cuevas Martínez <jccuevas@ujaen.es>
2019-10-28 11:09:10 +0100

    Carga inicial
    Inicio de la práctica

---
+++ Practica2/protocol.h
@@ -0,0 +1,34 @@
+#ifndef protocolostpte_practicas_headerfile
+#define protocolostpte_practicas_headerfile
+#endif
+
+// COMANDOS DE APLICACION
+#define SC "HELO"  // SOLICITUD DE CONEXION USER usuario 
+#define PW "MAIL FROM:"  // Password del usuario  PASS password
+
+#define SD  "QUIT"  // Finalizacion de la conexion de aplicacion
+#define SD2 "EXIT"  // Finalizacion de la conexion de aplicacion 
+#define ECHO "RCPT TO:" // Definicion del comando "ECHO" para el servicio de eco
+
+
+// RESPUESTAS A COMANDOS DE APLICACION
+#define OK  "OK"
+#define ER  "ER"
+
+//FIN DE RESPUESTA
+#define CRLF "\r\n"
+
+//ESTADOS
+#define S_HELO 0
+#define S_USER 1
+#define S_PASS 2
+#define S_DATA 3
+#define S_QUIT 4
+#define S_EXIT 5
+
+//PUERTO DEL SERVICIO
+#define TCP_SERVICE_PORT	25
+
+// NOMBRE Y PASSWORD AUTORIZADOS
+#define USER		"alumno"
+#define PASSWORD	"123456"
